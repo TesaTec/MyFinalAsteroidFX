50d9c7faafcd59a6597e1af16f8bc12727c80812
Magnus Hende Christensen <magnushende@gmail.com>
2025-05-21 00:07:54 +0200

    Made it work with Spring
    Missing to use the autowired

--- Core/src/main/java/dk/sdu/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/cbse/main/Main.java
@@ -1,168 +1,23 @@
 package dk.sdu.cbse.main;
 
 
-import dk.sdu.cbse.common.components.GraphicsComponent;
-import dk.sdu.cbse.common.components.TransformComponenet;
-import dk.sdu.cbse.common.data.Entity;
-import dk.sdu.cbse.common.data.GameData;
-import dk.sdu.cbse.common.data.GameKeys;
-import dk.sdu.cbse.common.data.World;
-import dk.sdu.cbse.common.services.IEntityProcessingService;
-import dk.sdu.cbse.common.services.IGamePluginService;
-import dk.sdu.cbse.common.services.IPostEntityProcessingService;
-
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import static java.util.stream.Collectors.toList;
-
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main extends Application {
-
-    private final GameData gameData = new GameData();
-    private final World world = new World();
-    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
-    private final Pane gameWindow = new Pane();
 
     public static void main(String[] args) {
         launch(Main.class);
     }
 
+
     @Override
     public void start(Stage window) throws Exception {
-        Text text = new Text(10, 20, "Destroyed asteroids: 0");
-        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        gameWindow.getChildren().add(text);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ModuleConfig.class);
 
-        Scene scene = new Scene(gameWindow);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.LEFT) || event.getCode().equals(KeyCode.A)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, true);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT) || event.getCode().equals(KeyCode.D)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, true);
-            }
-            if (event.getCode().equals(KeyCode.UP) || event.getCode().equals(KeyCode.W)) {
-                gameData.getKeys().setKey(GameKeys.UP, true);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, true);
-            }
-        });
-        scene.setOnKeyReleased(event -> {
-            if (event.getCode().equals(KeyCode.LEFT) || event.getCode().equals(KeyCode.A)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, false);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT) || event.getCode().equals(KeyCode.D)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, false);
-            }
-            if (event.getCode().equals(KeyCode.UP) || event.getCode().equals(KeyCode.W)) {
-                gameData.getKeys().setKey(GameKeys.UP, false);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, false);
-            }
-
-        });
-
-        // Lookup all Game Plugins using ServiceLoader
-        for (IGamePluginService iGamePlugin : getPluginServices()) {
-            iGamePlugin.start(gameData, world);
-        }
-        for (Entity entity : world.getEntities()) {
-            GraphicsComponent gc = entity.getComponent(GraphicsComponent.class);
-            Polygon polygon = new Polygon(gc.getPolygonCoordinates());
-
-            polygons.put(entity, polygon);
-            gameWindow.getChildren().add(polygon);
-        }
-        render();
-        scene.setFill(Color.BLACK);
-        window.setScene(scene);
-        window.setTitle("ASTEROIDS");
-        window.show();
-    }
-
-    private void render() {
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                update();
-                draw();
-                gameData.getKeys().update();
-            }
-
-        }.start();
-    }
-
-    private void update() {
-        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
-            entityProcessorService.process(gameData, world);
-        }
-        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
-            postEntityProcessorService.process(gameData, world);
-        }       
-    }
-
-    private void draw() {
-        for (Entity polygonEntity : polygons.keySet()) {
-
-
-            if(!world.getEntities().contains(polygonEntity)){
-                Polygon removedPolygon = polygons.get(polygonEntity);
-                polygons.remove(polygonEntity);
-                gameWindow.getChildren().remove(removedPolygon);
-            }
-        }
-
-        gameWindow.getChildren().clear();
-
-        polygons.clear();
-
-        List<Entity> entities = new ArrayList<>(world.getEntities());
-
-        entities.sort(Comparator.comparingInt(entity -> {
-            var graphics = entity.getComponent(GraphicsComponent.class);
-            return graphics != null ? graphics.getLayer().getValue() : Integer.MAX_VALUE;
-        }));
-
-        for (Entity entity : entities) {
-            TransformComponenet transCP = entity.getComponent(TransformComponenet.class);
-            GraphicsComponent graphicsCP = entity.getComponent(GraphicsComponent.class);
-            if(transCP == null || graphicsCP == null) {
-                continue;
-            }
-            Polygon polygon = new Polygon(graphicsCP.getPolygonCoordinates());
-            polygon.setTranslateX(transCP.getX());
-            polygon.setTranslateY(transCP.getY());
-            polygon.setRotate(transCP.getRotation());
-            polygon.setFill(graphicsCP.getEntityColor());
-
-            polygons.put(entity, polygon);
-
-            gameWindow.getChildren().add(polygon);
-
-        }
-
-    }
-
-    private Collection<? extends IGamePluginService> getPluginServices() {
-        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+        Game game = ctx.getBean(Game.class);
+        game.start(window);
+        game.render();
     }
 }
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -5,7 +5,13 @@ module Core {
     requires CommonAsteroid;
     requires CommonBullet;
     requires CommonStar;
-    opens dk.sdu.cbse.main to javafx.graphics;
+
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+
+    exports dk.sdu.cbse.main;
+    opens dk.sdu.cbse.main to javafx.graphics, spring.core;
     uses dk.sdu.cbse.common.services.IGamePluginService;
     uses dk.sdu.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.cbse.common.services.IPostEntityProcessingService;
